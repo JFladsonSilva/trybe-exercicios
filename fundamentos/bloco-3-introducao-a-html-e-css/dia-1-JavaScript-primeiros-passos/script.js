e4ea6c980781a46df0c1d1e05603831a0747ea69
JFladsonSIlva <fladsonsilva1@gmail.com>
2022-09-21 18:52:23 -0300

    exercicio concluído

---
+++ fundamentos/bloco-3-introducao-a-html-e-css/dia-1-JavaScript-primeiros-passos/script.js
@@ -0,0 +1,7 @@
+const a = 10
+const b = 20
+console.log(a + b + " é o resultado da adição")
+console.log(a - b + " é o resultado da subtração")
+console.log(a * b + " é o resultado da multiplicação")
+console.log(a / b + " é o resultado da divisão")
+console.log(a % b + " é o resto da divisão")
