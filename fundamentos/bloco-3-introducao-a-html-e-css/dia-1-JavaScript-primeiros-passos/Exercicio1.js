d95d87897c1e8c96031cdaf431db329e5f221982
JFladsonSIlva <fladsonsilva1@gmail.com>
2022-09-21 19:59:32 -0300

    resolvido até o exercicio 4

--- fundamentos/bloco-3-introducao-a-html-e-css/dia-1-JavaScript-primeiros-passos/Exercicio1.js
+++ fundamentos/bloco-3-introducao-a-html-e-css/dia-1-JavaScript-primeiros-passos/Exercicio1.js
@@ -1,6 +1,6 @@
 const a = 10
 const b = 20
-console.log(a + b + " é o resultado da adição")
+console.log( " é o resultado da adição " + ( a + b))
 console.log(a - b + " é o resultado da subtração")
 console.log(a * b + " é o resultado da multiplicação")
 console.log(a / b + " é o resultado da divisão")
---
+++ fundamentos/bloco-3-introducao-a-html-e-css/dia-1-JavaScript-primeiros-passos/exercicio2.js
@@ -0,0 +1,28 @@
+// const a = 15
+// const b = 7
+// EXERCICIO2
+// if (a > b){
+//     console.log(a)
+// } else{
+//     console.log(b)
+// }
+
+// EXERCICIO3
+//  const c = 10
+// if (a > b && a > c){
+//     console.log(a)
+// } else if (b > a && b > c){
+//     console.log(b)
+// } else {
+//     console.log(c)
+// }
+
+// EXERCICIO4
+// const num = 15
+// if (num > 0){
+//     console.log("positive")
+// } else if (num < 0) {
+//     console.log("negative")
+// } else {
+//     console.log(0)
+// }
