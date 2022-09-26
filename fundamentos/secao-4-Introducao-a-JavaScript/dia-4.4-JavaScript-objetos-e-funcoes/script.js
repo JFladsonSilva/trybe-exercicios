37e5dd72996a45b8ab598f1952e8ee5e63b66a70
JFladsonSIlva <fladsonsilva1@gmail.com>
2022-09-26 19:03:05 -0300

    exercicios finalizados no momento

--- fundamentos/secao-4-Introducao-a-JavaScript/dia-4.4-JavaScript-objetos-e-funcoes/script.js
+++ fundamentos/secao-4-Introducao-a-JavaScript/dia-4.4-JavaScript-objetos-e-funcoes/script.js
@@ -35,10 +35,51 @@
 // let age = 34;
 // let medals = { golden: 2, silver: 3 };
 
-let player = {
-    name:'Marta',
-    lastName: 'Silva',
-    age: 34,
-    medals: { golden:2, silver: 3}
-}  
-console.table(player)
+// let player = {
+//     name:'Marta',
+//     lastName: 'Silva',
+//     age: 34,
+//     medals: { golden:2, silver: 3}
+// }  
+// console.table(player)
+
+// let cars = ['Saab', 'Volvo', 'BMW'];
+
+// for (let index in cars) {
+//   console.log(cars[index]);
+// }
+
+// let car = {
+//     type: 'Fiat',
+//     model: '500',
+//     color: 'white',
+//   };
+  
+//   for (let index in car) {
+//     console.log(index, car[index]);
+//   }
+// Usando o objeto abaixo, utilize for/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge',
+//   };
+//   for (let index in names){
+//     console.log(names[index])
+//   }
+
+//   Usando o objeto abaixo, utilize for/in e imprima um console.log com as chaves e valores desse objeto.
+// let car = {
+//     model: 'A3 Sedan',
+//     manufacturer: 'Audi',
+//     year: 2020
+//   };
+//   for(let index in car){
+//     console.log(car[index])
+//   }
+const object = { a: 1, b: 2, c: 3 };
+
+for (const property in object) {
+  console.log(property + ' = ' + object[property]);
+}
