5199724a0ecdbd3220418276a217b23fa0f83384
JFladsonSIlva <fladsonsilva1@gmail.com>
2022-09-26 10:35:41 -0300

    exercicios

--- fundamentos/bloco-3-introducao-a-html-e-css/dia-1-JavaScript-primeiros-passos/exercicio2.js
+++ fundamentos/bloco-3-introducao-a-html-e-css/dia-1-JavaScript-primeiros-passos/exercicio2.js
@@ -52,20 +52,41 @@
 //         break;
 //     }
 //EXERCICIO7
-let nota = 90
+// let nota = 90
 
-if (nota >= 90 && nota < 100){
-    console.log('Nota A')
-} else if (nota >= 80 && nota < 90){
-    console.log('Nota B')
-} else if (nota >= 70 && nota < 80){
-    console.log('Nota C')
-} else if (nota >= 60 && nota < 70){
-    console.log('Nota D')
-} else if (nota >= 50 && nota < 60) {
-    console.log('Nota E')
-} else if (nota >= 40 && nota < 50){
-    console.log('Nota F')
-} else if (nota < 0 || nota > 100) {
-    console.log('Nota inválida')
+// if (nota >= 90 && nota < 100){
+//     console.log('Nota A')
+// } else if (nota >= 80 && nota < 90){
+//     console.log('Nota B')
+// } else if (nota >= 70 && nota < 80){
+//     console.log('Nota C')
+// } else if (nota >= 60 && nota < 70){
+//     console.log('Nota D')
+// } else if (nota >= 50 && nota < 60) {
+//     console.log('Nota E')
+// } else if (nota >= 40 && nota < 50){
+//     console.log('Nota F')
+// } else if (nota < 0 || nota > 100) {
+//     console.log('Nota inválida')
+// }
+//EXERCICIO8
+// const n1 = 4
+// const n2 = 1
+// const n3 = 2
+// if (n1 % 2 == 1 || n2 % 2 == 1 || n2 % 2 == 1){
+//     console.log(true)
+// }else {
+//     console.log(false)
+// }
+//EXERCICIO9
+const custoProduto = 50;
+const valorVenda = 100;
+const impostoSobreOCusto = 0.2 * 100;
+const valorCustoTotal = custoProduto + impostoSobreOCusto;
+const lucro = valorVenda*1000 - valorCustoTotal;
+
+if (lucro > 0){
+    console.log('O lucro foi de ' + lucro);
+}else if (valorVenda < 0 || valorCustoTotal < 0){
+    console.log('Valores inválidos');
 }
