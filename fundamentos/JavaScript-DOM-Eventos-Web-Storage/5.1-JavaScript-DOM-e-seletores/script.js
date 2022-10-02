928c169ea3ca19a3964a346c7b083abe80de89f2
JFladsonSIlva <fladsonsilva1@gmail.com>
2022-10-02 17:08:20 -0300

    iniciando exercicios

---
+++ fundamentos/JavaScript-DOM-Eventos-Web-Storage/5.1-JavaScript-DOM-e-seletores/script.js
@@ -0,0 +1,6 @@
+const header = document.getElementById('header-container');
+header.style.backgroundColor = 'bisque';
+
+const section1 = documment.getElementsByClassName('emergency-tasks'); 
+section1.style.backgroundColor = 'bisque'
+
