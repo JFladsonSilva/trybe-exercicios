f86e164bfd4ce240bb6edc8734972ae8e5fc7ebb
JFladsonSIlva <fladsonsilva1@gmail.com>
2022-11-21 18:45:48 -0300

    Anotações

--- front-end/front-end-S1/dia-2-javaScript-Assíncrono-Promises-fetch/Anotacoes.js
+++ front-end/front-end-S1/dia-2-javaScript-Assíncrono-Promises-fetch/Anotacoes.js
@@ -1,3 +1,5 @@
 // Esse construtor recebe uma função como parâmetro e essa função deverá ter dois argumentos: a função resolve e a função reject.
 new Promise((resolve, reject) => {});
 
+.then
+// é utilizado encadeado com uma promisse, ele recebe uma função como argumento e essa função recebe o argumento Response, o response é o retorno da Promise.
--- front-end/front-end-S1/dia-2-javaScript-Assíncrono-Promises-fetch/main.js
+++ front-end/front-end-S1/dia-2-javaScript-Assíncrono-Promises-fetch/main.js
@@ -8,4 +8,17 @@ const resolvedPromise = () =>
       const randomNumber = generateRandomNumber();
       resolve(randomNumber);
     }, 1000);
-  });
+  });  
+ 
+  // o then basicamente é uma função para retornar o valor de uma promise com o resultado resolved.
+  resolvedPromise().then((response) => {
+    console.log(response);
+  })
+
+  /////////////////////////////////////////////////////////////////////////
+  fetch("https://api.goprogram.ai/inspiration")
+  .then(response => response.json())
+  .then(data => console.log(`"${data.quote}" | ${data.author}`));
+  //De olho na dica 👀: Você reparou que usamos dois .then seguidos após fazer o fetch? Esse processo é chamado de encadeamento. Podemos usar esse recurso retornando um valor dentro de um .then. O valor retornado será recebido pelo próximo .then encadeado. No caso acima, estamos retornando o valor de response.json() para o próximo .then, que recebe o valor através da variável data.
+
+  //Anota aí 📝: Você pode ter quantos then encadeados forem necessários, mas geralmente costuma-se ter apenas um catch, pois esse catch conseguirá capturar um erro que seja disparado em qualquer um dos then anteriores.
