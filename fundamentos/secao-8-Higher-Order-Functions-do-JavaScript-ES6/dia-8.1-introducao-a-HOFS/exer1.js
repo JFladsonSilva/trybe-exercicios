1aa59316c022f10da880f25c7facacd762e4764f
JFladsonSIlva <fladsonsilva1@gmail.com>
2022-11-29 18:30:21 -0300

    commit initial

---
+++ fundamentos/secao-8-Higher-Order-Functions-do-JavaScript-ES6/dia-8.1-introducao-a-HOFS/exer1.js
@@ -0,0 +1,15 @@
+const emailListInData = [
+    'roberta@email.com',
+    'paulo@email.com',
+    'anaroberta@email.com',
+    'fabiano@email.com',
+  ];
+  
+  // Adicione seu código aqui
+  emailListInData.forEach((email) => {
+    console.log(`O email ${email} está cadastrado em nosso banco de dados!`);
+  })
+
+  //Anotações
+  //some = algum. Se algum elemento do array satisfaz uma condição, retorna true. Se não satisfaz retorna false.
+  //every = se todos os elementos de um array satisfazem uma condição, retorna true. se não satisfaz retorna false
