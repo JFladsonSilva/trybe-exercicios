e1a2a7180438d21620db298568e92a182f89d600
JFladsonSIlva <fladsonsilva1@gmail.com>
2023-01-10 09:21:37 -0300

    first commit after class change

---
+++ fundamentos/secao-8-Higher-Order-Functions/dia-8.5-Array/exercise1.js
@@ -0,0 +1,17 @@
+// Exercício 1
+// Escreva a função swap, que, dado um array de 3 elementos, retorna um novo array com o primeiro e terceiro elementos trocados. Detalhe: você precisa fazer essa função gastando apenas 1 linha. O retorno da função utilizando o array myList deve ser [3, 2, 5].
+
+const myList = [5, 2, 3];
+
+// escreva swap abaixo
+const swap = ([first, second, third]) => [third, second, first]
+// console.log(swap(myList));
+
+// Exercício 2
+// Suponha que você esteja trabalhando em projeto de um site de carros, onde cada carro é representado dentro de um array. Então sua liderança pede que seja mudado o formato de array para objeto. Para isso, crie uma função chamada toObject que, dada uma lista, retorna um objeto representando o carro:
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+// escreva toObject abaixo
+const toObject
